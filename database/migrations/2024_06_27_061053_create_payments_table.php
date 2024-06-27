9f4e7574763dbc35c681c9178b7482ed4288a25a
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-27 06:12:58 +0000

    Create payment model and corresponding migration

---
+++ database/migrations/2024_06_27_061053_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->enum('type', ['credit_card', 'cash_on_delivery', 'bank_transfer']);
+            $table->json('details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
