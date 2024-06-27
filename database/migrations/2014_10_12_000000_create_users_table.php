c84e1150d4aa1c2b09de4b9add1f7943bc3fe78e
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-27 05:42:15 +0000

    Fix typo in users migration: correct 'boolen' to 'boolean'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
                 $table->uuid('uuid')->unique();
                 $table->string('first_name');
                 $table->string('last_name');
-                $table->boolen('is_admin')->default(false);
+                $table->boolean('is_admin')->default(false);
                 $table->string('email')->unique();
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
