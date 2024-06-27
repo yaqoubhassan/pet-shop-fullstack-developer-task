9abc8766549696db56382ec60ef5294fafd3e00f
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-27 05:45:13 +0000

    Create product model and corresponding migration

---
+++ database/migrations/2024_06_26_230644_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('category_uuid');
+            $table->foreign('category_uuid')->references('uuid')->on('categories')->onDelete('cascade');
+            $table->uuid('uuid')->unique();
+            $table->string('title');
+            $table->decimal('price', 10, 2);
+            $table->text('description');
+            $table->json('metadata')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
