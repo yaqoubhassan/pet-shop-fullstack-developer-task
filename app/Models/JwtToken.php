2b502f89ce429d0fa083ef970d9f72ab27fa34fa
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-28 19:11:47 +0000

    Update the `JwtToken` model to include all fillable fields

--- app/Models/JwtToken.php
+++ app/Models/JwtToken.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class JwtToken extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'unique_id',
+        'token_title',
+        'restrictions',
+        'permissions',
+        'expires_at',
+        'last_used_at',
+        'refreshed_at',
+    ];
 }
