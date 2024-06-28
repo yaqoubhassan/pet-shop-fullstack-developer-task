bf2b8eff7c99c925652d5a66caadef838b977b28
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-28 19:10:53 +0000

    Create a custom request for validating user creation inputs.

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,',
+            'password' => 'required|string|min:8|confirmed',
+            'address' => 'required|string',
+            'phone_number' => 'required|string|max:15',
+            'is_marketing' => 'boolean',
+            'is_admin' => 'boolean',
+            'avatar' => 'nullable|file|mimes:jpg,jpeg,png,bmp|max:2048',
+        ];
+    }
+}
