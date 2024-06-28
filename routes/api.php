75cd1993b931a24b3654cd4c0dbc0b75ce330470
Yakubu Alhassan <yaqoubdramani@gmail.com>
2024-06-28 19:12:45 +0000

    Add the create user endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('v1/user/create', [UserController::class, 'store'])->name('user.create');
